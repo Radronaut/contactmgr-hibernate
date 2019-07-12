ec3a3d2e642c31b3b862a9239b997a1691cac0b0
Justin Carroll <JWCarroll88@gmail.com>
2019-07-12 07:55:20 -0500

    Initial Commit

---
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -0,0 +1,22 @@
+package com.teamtreehouse.contactmgr;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
+
+public class Application {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
