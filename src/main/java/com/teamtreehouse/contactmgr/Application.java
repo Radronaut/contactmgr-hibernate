29c371099982c21a5c6490933699c4aa5d96b855
Justin Carroll <JWCarroll88@gmail.com>
2019-07-29 07:57:48 -0500

    Fixed JAX-B issue

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -10,13 +10,15 @@ import org.hibernate.service.ServiceRegistry;
 
 
 public class Application {
+    // Hold a reusable reference to a SessionFactory (since we need only one)
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistry
         final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         return new MetadataSources(registry).buildMetadata().buildSessionFactory();
-
     }
+
 
     public static void main(String[] args) {
         Contact contact = new Contact.ContactBuilder("Chris","Ramacciotti")
